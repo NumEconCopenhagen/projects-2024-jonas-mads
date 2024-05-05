347752edb0df514301b5e75bbf6a9799ee3b9245
tvp211 <164737952+tvp211@users.noreply.github.com>
2024-05-05 13:57:12 +0200

    Update InauguralProject_Final.py

--- inauguralproject/InauguralProject_Final.py
+++ inauguralproject/InauguralProject_Final.py
@@ -99,12 +99,30 @@ i = 0
 # While loop that creates the prices and puts it in the list prices
 while 0.5 + 2 * i / N <= 2.5:
     prices.append(0.5 + 2 * i / N)
-    i+=1
+    i += 1
+
+# Lists to store errors
+eps1_values = []
+eps2_values = []
 
 # Calculates the error in the market clearing conditions eps1 and eps2 for each price
 for p in prices:
     eps1, eps2 = model.check_market_clearing(p)
-    print("p1 = " + str(round(p,3)) + ". Epsilon 1 = " + str(round(eps1,3)) + ". Epsilon 2 = " + str(round(eps2,3)))
+    eps1_values.append(eps1)
+    eps2_values.append(eps2)
+
+# Plotting the errors against prices
+plt.plot(prices, eps1_values, label='Epsilon 1')
+plt.plot(prices, eps2_values, label='Epsilon 2')
+
+# Adding labels and title
+plt.xlabel('Price (p1)')
+plt.ylabel('Error')
+plt.title('Market Clearing Errors')
+plt.legend()
+
+# Display the plot
+plt.show()
 
 ############################################
 # Assignment 3
@@ -119,14 +137,11 @@ def market_clearing_equation(p1):
 # Find the root of the market clearing equation where eps1 - eps2 = 0
 result = root_scalar(market_clearing_equation, bracket=[0.5, 1])
 
-# Check if the root finding was successful
-if result.converged:
-    p1_market_clearing = result.root
-else:
-    print("Root finding did not converge.")
+p1_market_clearing = result.root
 
 p1_3 =  p1_market_clearing 
 print("Market clearing price (p1) = " + str(p1_3))
+
 
 ############################################
 # Assignment 4a
@@ -154,8 +169,12 @@ x1A_4a = max_uA_row['x1A']
 x2A_4a = max_uA_row['x2A']
 p1_4a = max_uA_row['p1']
 uA_4a = max_uA_row['uA']
+uB_4a = model.utility_B(1-x1A_4a, 1-x2A_4a)
 
-print("Max Utility Allocation (4a): x1A = " + str(x1A_4a) + ". x2A = " + str(x2A_4a))
+print("Max Utility Allocation (4a): x1A = {:.5f}. x2A = {:.5f}.".format(x1A_4a, x2A_4a))
+print("Max Utility for Agent A (4a): uA = {:.5f}".format(uA_4a))
+print("Max Utility for Agent B (4a): uB = {:.5f}".format(uB_4a))
+print("Optimal p1 (4a): {:.5f}".format(p1_4a))
 
 ############################################
 # Assignment 4b
@@ -168,7 +187,7 @@ def negative_utility_A(p1):
     return -model.utility_A(1 - model.demand_B(p1)[0], 1 - model.demand_B(p1)[1])
 
 # Find the maximum utility for the given price
-result = minimize_scalar(negative_utility_A, bounds=(1e-6, None))  # Set a minimum bound for p1
+result = minimize_scalar(negative_utility_A, bounds=[1e-6, None])  # Set a minimum bound for p1
 
 # Check if optimization was successful
 if result.success:
@@ -177,14 +196,20 @@ if result.success:
 else:
     print("Optimization failed:", result.message)
 
-x1A, x2A = model.demand_A(optimal_p1)
+
+x1A = (1-model.demand_B(optimal_p1)[0])
+x2A = (1-model.demand_B(optimal_p1)[1])
 
 x1A_4b = x1A
 x2A_4b = x2A
 p1_4b = optimal_p1
 uA_4b = max_uA
+uB_4b = model.utility_B(1-x1A_4b, 1-x2A_4b)
 
-print("Max Utility Allocation (4b): x1A = " + str(x1A_4b) + ". x2A = " + str(x2A_4b))
+print("Max Utility Allocation (4b): x1A = {:.5f}. x2A = {:.5f}.".format(x1A_4b, x2A_4b))
+print("Max Utility for Agent A (4b): uA = {:.5f}".format(uA_4b))
+print("Max Utility for Agent B (4b): uB = {:.5f}".format(uB_4b))
+print("Optimal p1 (4b): {:.5f}".format(optimal_p1))
 
 ############################################
 # Assignment 5a
@@ -231,8 +256,11 @@ max_uA_row5 = maxUtility5.loc[max_uA_index5] # Extract the row with the maximum 
 x1A_5a = max_uA_row5['x1A']
 x2A_5a = max_uA_row5['x2A']
 uA_5a = max_uA_row5['uA']
+uB_5a = model.utility_B(1-x1A_5a, 1-x2A_5a)
 
-print("Max Utility Allocation (5a): x1A = " + str(x1A_5a) + ". x2A = " + str(x2A_5a))
+print("Max Utility Allocation (5a): x1A = {:.5f}. x2A = {:.5f}.".format(x1A_5a, x2A_5a))
+print("Max Utility for Agent A (5a): uA = {:.5f}".format(uA_5a))
+print("Max Utility for Agent B (5a): uB = {:.5f}".format(uB_5a))
 
 ############################################
 # Assignment 5b
@@ -249,10 +277,10 @@ def constraint_function(x):
     return model.utility_B(1 - x1A, 1 - x2A) - model.utility_B(w1B, w2B)
 
 # Define the bounds for x1A and x2A
-bounds = [(0, 1), (0, 1)]
+bounds = [[0, 1], [0, 1]]
 
 # Initial guess for x1A and x2A
-initial_guess = [0.5, 0.5]  # You may adjust this based on your problem domain
+initial_guess = [0.560, 0.853] # The initial guess is made from the result from 5a
 
 # Define the optimization problem with the constraint
 constraint = {'type': 'ineq', 'fun': constraint_function}
@@ -268,8 +296,11 @@ else:
 x1A_5b = optimal_x1A
 x2A_5b = optimal_x2A
 uA_5b = max_uA
+uB_5b = model.utility_B(1-x1A_5b, 1-x2A_5b)
 
-print("Max Utility Allocation (5b): x1A = " + str(x1A_5b) + ". x2A = " + str(x2A_5b))
+print("Max Utility Allocation (5b): x1A = {:.5f}. x2A = {:.5f}.".format(x1A_5b, x2A_5b))
+print("Max Utility for Agent A (5b): uA = {:.5f}".format(uA_5b))
+print("Max Utility for Agent B (5b): uB = {:.5f}".format(uB_5b))
 
 ############################################
 # Assignment 6a
@@ -299,8 +330,11 @@ else:
 x1A_6a = optimal_x1A
 x2A_6a = optimal_x2A
 uA_6a = max_uA
+uB_6a = model.utility_B(1-x1A_6a, 1-x2A_6a)
 
-print("Max Utility Allocation (6a): x1A = " + str(x1A_6a) + ". x2A = " + str(x2A_6a))
+print("Max Utility Allocation (6a): x1A = {:.5f}. x2A = {:.5f}.".format(x1A_6a, x2A_6a))
+print("Max Utility for Agent A (6a): uA = {:.5f}".format(uA_6a))
+print("Max Utility for Agent B (6a): uB = {:.5f}".format(uB_6a))
 
 ############################################
 # Assignment 6b
@@ -310,10 +344,14 @@ print("Max Utility Allocation (6a): x1A = " + str(x1A_6a) + ". x2A = " + str(x2A
 fig_new = plt.figure(frameon=False, figsize=(6,6), dpi=100)
 ax_sub = fig_new.add_subplot(1, 1, 1)
 
+ax_sub.set_xlabel("$x_1^A$")
+ax_sub.set_ylabel("$x_2^A$")
+
 # Scatter plot for each allocation
+ax_sub.scatter(xA_df['x1A'],xA_df['x2A'],marker='s',color='red',label='set C')
 ax_sub.scatter(par.w1A, par.w2A, marker='s', color='black', label='endowment')
-ax_sub.scatter(x1A_4a, x2A_4a, marker='s', color='red', label='Allocation - 4a')
-ax_sub.scatter(x1A_4b, x2A_4b, marker='s', color='green', label='Allocation - 4b')
+ax_sub.scatter(x1A_4a, x2A_4a, marker='o', color='orange', label='Allocation - 4a')
+ax_sub.scatter(x1A_4b, x2A_4b, marker='^', color='green', label='Allocation - 4b')
 ax_sub.scatter(x1A_5a, x2A_5a, marker='s', color='yellow', label='Allocation - 5a')
 ax_sub.scatter(x1A_5b, x2A_5b, marker='s', color='purple', label='Allocation - 5b')
 ax_sub.scatter(x1A_6a, x2A_6a, marker='s', color='gray', label='Allocation - 6a')
@@ -346,34 +384,16 @@ random.seed = '42'
 w1A_list = []
 w2A_list = []
 
-
-i = 0
-j = 0
-
 # Loops that generate random values between 0 and 1 for w1a and appends to the empty lists created before
 # Runs 2 times with j and 50 times with i
-while j < 2:
-    while i < 50:
-        random_number = random.random()
-        # Make sure the random number is not 0 or 1
-        while random_number == 0 or random_number == 1:
-            random_number = random.random()
-            
-            print("Random number between 0 and 1 (excluding 0 and 1):", random_number)
-            
+for j in range(2):
+    for i in range(50):
+        random_number = random.uniform(0, 1)
+        #print("Random number between 0 and 1 (excluding 0 and 1):", random_number)
         if j == 0:
             w1A_list.append(random_number)
         else:
             w2A_list.append(random_number)
-            
-        #print(random_number)
-        
-        i+=1
-
-    i=0
-    j+=1
-
-
 
 # Scatter plot
 plt.figure(figsize=(8, 6))
@@ -391,7 +411,6 @@ plt.legend()
 plt.show()
 
 
-
 ############################################
 # Assignment 8
 ############################################
@@ -404,7 +423,6 @@ beta = 2/3
 #w2B_list = [1 - w2A for w2A in w2A_list]
 
 # Sets the price equal to the market clearing price in assignment 3
-p1 = p1_market_clearing
 x1A_list = []
 x2A_list = []
 
@@ -414,13 +432,23 @@ x2A_list = []
 i = 0
 while i < 50:
     
+    # Define a function that calculates eps1 and eps2 for a given p1
+    def market_clearing_equation(p1):
+        eps1, eps2 = model.market_clearing_new_endowments(p1,w1A_list[i],w2A_list[i]) # Call the model's function to calculate eps1 and eps2
+        return eps1 - eps2 # Return the difference between eps1 and eps2
+
+    # Find the root of the market clearing equation where eps1 - eps2 = 0
+    result = root_scalar(market_clearing_equation, bracket=[0.5, 2.5])
+
+    # Check if the root finding was successful
+    if result.converged:
+        p1 = result.root
+           
     x1A = alpha * (p1 * w1A_list[i] + 1 * w2A_list[i]) / (p1)
     x2A = (1 - alpha) * (p1 * w1A_list[i] + 1 * w2A_list[i]) / 1
     
     x1A_list.append(x1A)
     x2A_list.append(x2A)
-    
-    #print(x1A)
     
     
     i+=1
@@ -444,13 +472,17 @@ ax_new.set_ylabel("$x_2^A$")
 ax_new.set_xlim([-0.1, w1bar + 0.1])
 ax_new.set_ylim([-0.1, w2bar + 0.1])
 
-# Create a twinx and twiny for secondary axes
-temp_new = ax_new.twinx()
-temp_new.set_ylabel("$x_2^B$")
-ax_B_new = temp_new.twiny()
+# Create secondary axes
+ax_B_new = ax_new.twiny()
 ax_B_new.set_xlabel("$x_1^B$")
-ax_B_new.invert_xaxis()
-ax_B_new.invert_yaxis()
+ax_B_new.set_xlim(ax_new.get_xlim())  # Set the same x limits as primary axes
+ax_B_new.invert_xaxis()  # Invert the x-axis to match the primary axes
+
+ax_B_new_y = ax_new.twinx()
+ax_B_new_y.set_ylabel("$x_2^B$")
+ax_B_new_y.set_ylim(ax_new.get_ylim())  # Set the same y limits as primary axes
+ax_B_new_y.invert_yaxis()  # Invert the y-axis to match the primary axes
+
 
 # Add legend
 ax_new.legend(frameon=True, loc='upper right', bbox_to_anchor=(1.6, 1.0))
